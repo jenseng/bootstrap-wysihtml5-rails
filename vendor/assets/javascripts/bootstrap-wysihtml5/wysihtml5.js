932e43a87bea9eccfc634735bf0419a7e002c651
Jon Jensen <jenseng@gmail.com>
2014-04-04 17:34:11 -0600

    arbitrary class and inline style support
    now you can create all the ugly markup you want like so:
    
     parseRules: {
       classes: {
         '*': 1 // well that's not too bad i guess
       },
       tags: {
         div: {
           check_attributes: {style: "style"} // zomg can o' worms
         }
       }
     }

--- vendor/assets/javascripts/bootstrap-wysihtml5/wysihtml5.js
+++ vendor/assets/javascripts/bootstrap-wysihtml5/wysihtml5.js
@@ -4960,7 +4960,7 @@ wysihtml5.dom.parse = (function() {
     classesLength = classes.length;
     for (; i<classesLength; i++) {
       currentClass = classes[i];
-      if (allowedClasses[currentClass]) {
+      if (allowedClasses['*'] || allowedClasses[currentClass]) {
         newClasses.push(currentClass);
       }
     }
@@ -5077,6 +5077,13 @@ wysihtml5.dom.parse = (function() {
       return function(attributeValue) {
         attributeValue = (attributeValue || "").replace(REG_EXP, "");
         return attributeValue || null;
+      };
+    })(),
+
+    style: (function() {
+      var REG_EXP = /[^a-z0-9_\-]\:[^a-z0-9_\-];/gi;
+      return function(attributeValue) {
+        return attributeValue;
       };
     })()
   };
